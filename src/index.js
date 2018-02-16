7612485c759cd6fe52878eb23ba95aef1ef7c788
Shay Davidson <sdavidson@paypal.com>
2018-02-16 15:08:03 +0200

    Basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-import "index.css";
+let config = {
+  spaceColor: "#000000",
+  rngSeed: 1000
+};
+
+function updateInputs() {
+  Object.keys(config).forEach(key => {
+    document.getElementById(key).value = config[key];
+  });
+}
+
+function render() {
+  console.log("render");
+}
+
+function subscribeToInputChanges() {
+  Array.from(document.getElementsByTagName("input")).forEach(element => {
+    element.addEventListener("change", ev => {
+      config[ev.target.id] = ev.target.value;
+      render();
+    });
+  });
+}
+
+updateInputs();
+subscribeToInputChanges();
